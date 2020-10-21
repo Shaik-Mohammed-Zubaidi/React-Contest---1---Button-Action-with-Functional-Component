819fb4393fa55b1347948247cdf44801673de947
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-21 21:04:31 +0530

    commit1fn

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paratext, setText] = useState("");
+  const handleclick = () => {};
   return (
     <div id="main">
-      // Do not alter the main div
+      <button
+        id="click"
+        onClick={() =>
+          setText(
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          )
+        }
+      >
+        Click
+      </button>
+      <p id="para">{paratext}</p>
     </div>
   );
 }
-
 
 export default App;
