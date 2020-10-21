6c9c16d8d0a799c2eb12914d6dc90e1c6001bc04
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-21 21:35:16 +0530

    commit2fn

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,23 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [vis, setText] = React.useState(false);
+  const [vis, setVis] = React.useState(false);
   // let vis = false;
   const handleclick = () => {
     // vis = true;
-    setText(true);
+    setVis(true);
   };
   return (
     <div id="main">
       <button id="click" onClick={() => handleclick()}>
         Output
       </button>
-      {vis && (
+      {vis ? (
         <p id="para">
           "Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy"
         </p>
-      )}
+      ) : null}
     </div>
   );
 }
